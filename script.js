9368ac27cdf45b0729a16be6ea2a87db3da5fe2d
chinna222 <gondigangadhar@gmail.com>
2024-01-17 21:54:48 +0530

    video recording and downloading done

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let video = document.querySelector("video");
+const recordBtn = document.getElementById("record-btn");
+const captureBtn = document.getElementById("capture-btn");
+let mediaRecorder;
+let chunks = [];
 
 let constraints = {
   video: true,
@@ -7,4 +11,39 @@ let constraints = {
 
 navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
   video.srcObject = stream;
+  mediaRecorder = new MediaRecorder(stream);
+  mediaRecorder.ondataavailable = (e) => {
+    chunks.push(e.data);
+  };
+  recordBtn.onclick = recordBtnClickHandler;
+  mediaRecorder.onstop = stopHandler;
 });
+
+const stopHandler = () => {
+  const blob = new Blob(chunks, {
+    type: "video/webm",
+  });
+  chunks = [];
+  const url = URL.createObjectURL(blob);
+  // Create a temporary link and trigger the download
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = "recorded-video.webm";
+  a.click();
+
+  // Cleanup
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+};
+
+const recordBtnClickHandler = (ß) => {
+  if (mediaRecorder.state === "recording") {
+    mediaRecorder.stop();
+    recordBtn.classList.remove("scale-record");
+  } else {
+    mediaRecorder.start();
+    recordBtn.classList.add("scale-record");
+  }
+};
